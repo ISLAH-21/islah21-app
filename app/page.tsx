670c6717097123a05f55180b3aa48da3d138ac4b
Abdullah Ammar <opowae909@gmail.com>
2025-05-06 23:24:14 +0700

    fix: add cache

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {
 	getAlumniSchemaParams,
 } from "@/services/alumni/alumni-schema";
 import { getAlumni } from "@/services/alumni/get-alumni";
+import { unstable_cache } from "next/cache";
 
 const getAlumniList = async ({ params }: { params: GetAlumniSchemaParams }) => {
 	const safeParams = getAlumniSchemaParams.safeParse(params);
@@ -35,13 +36,18 @@ const getAlumniList = async ({ params }: { params: GetAlumniSchemaParams }) => {
 	};
 };
 
+const getAlumniListCached = unstable_cache(getAlumniList, ["alumni-list"], {
+	revalidate: 3600,
+	tags: ["alumni-list"],
+});
+
 export default async function Home({
 	params,
 }: {
 	params: Promise<GetAlumniSchemaParams>;
 }) {
 	const queryParams = await params;
-	const { data: alumni } = await getAlumniList({ params: queryParams });
+	const { data: alumni } = await getAlumniListCached({ params: queryParams });
 
 	return (
 		<main className="container mx-auto px-4 py-8">
